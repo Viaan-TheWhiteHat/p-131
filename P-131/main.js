00faf2c5ff9ad1d6ac8675ff949bba7ed3927d1d
Viaan-TheWhiteHat <79201716+Viaan-TheWhiteHat@users.noreply.github.com>
2021-11-17 15:56:47 +0530

    Add files via upload

---
+++ P-131/main.js
@@ -0,0 +1,19 @@
+function cake()
+{
+    window.location.replace("cake.html");
+}
+
+function nature()
+{
+    window.location.replace("nature.html");
+}
+
+function laptop()
+{
+    window.location.replace("laptop.html");
+}
+
+function bottle()
+{
+    window.location.replace("bottle.html");
+}
